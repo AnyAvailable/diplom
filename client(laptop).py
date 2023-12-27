5848f669c2c907faf19cd60ebaa1e8a6f1d16751
AnyAvailable <egorov_viacheslav@outlook.com>
2023-12-27 14:12:48 +0300

    test version of diplom

---
+++ client(laptop).py
@@ -0,0 +1,17 @@
+"""
+A simple Python script to send messages to a sever over Bluetooth using
+Python sockets (with Python 3.3 or above).
+"""
+
+import socket
+
+serverMACAddress = 'e8:48:b8:c8:20:00'
+port = 3
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.connect((serverMACAddress,port))
+while 1:
+    text = input()
+    if text == "quit":
+        break
+    s.send(bytes(text, 'UTF-8'))
+s.close()
