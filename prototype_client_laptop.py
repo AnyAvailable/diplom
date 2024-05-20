fd4f621dd6bdb4d66af30c623c0a1c7512e92d84
AnyAvailable <105864269+AnyAvailable@users.noreply.github.com>
2024-05-20 14:18:55 +0300

    bug fix

--- prototype_client_laptop.py
+++ prototype_client_laptop.py
@@ -9,10 +9,10 @@ class App(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('gui.ui', self)
-        self.win.clicked.connect(self.win)
+        self.win_vk.clicked.connect(self.win)
 
     def win(self):
-        serverMACAddress = 'e8:48:b8:c8:20:00'
+        """serverMACAddress = 'e8:48:b8:c8:20:00'
         port = 5
         s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
         s.connect((serverMACAddress,port))
@@ -23,7 +23,8 @@ class App(QMainWindow):
             print(bytes.decode(data, "utf-8") + "pressed")
             time.sleep(1)
             break
-        s.close()
+        s.close()"""
+        pass
 
 
 if __name__ == "__main__":
